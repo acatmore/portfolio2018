4da8f5960cac00989e1c19123d5fc578b7cfc0bb
Alex Atmore <acatmore@gmail.com>
2018-06-01 14:12:39 -0700

    adding images and portfolio text

--- gatsby_portfolio/src/components/Footer.js
+++ gatsby_portfolio/src/components/Footer.js
@@ -1,13 +1,29 @@
 import React from 'react'
 
-const Footer = (props) => (
-    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
-    </footer>
+const Footer = props => (
+  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
+    <p className="copyright">
+      <a
+        href="https://twitter.com/acatmore"
+        target="_blank"
+        className="icon fa-twitter"
+      />
+      <a
+        href="https://github.com/acatmore"
+        target="_blank"
+        className="icon fa-github"
+      />
+      <a
+        href="https://www.linkedin.com/in/alex-atmore-8b38b053"
+        target="_blank"
+        className="icon fa-linkedin"
+      />
+    </p>
+  </footer>
 )
 
 Footer.propTypes = {
-    timeout: React.PropTypes.bool
+  timeout: React.PropTypes.bool,
 }
 
 export default Footer
